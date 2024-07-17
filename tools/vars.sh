119aa846ca21e18a702503259b9d5cedabf11a28
Иван Мороз <github@maroz.by>
2024-07-17 12:00:37 +0300

    Рефакторинг скрипта установки vmagent

---
+++ tools/vars.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+if [[ "$OS" == "" ]]; then
+  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    OS="linux"
+  elif [[ "$OSTYPE" == "darwin"* ]]; then
+    OS="darwin"
+  else
+    echo "Unknown OS"
+    exit 1
+  fi
+fi
+
+if [[ "$ARCH" == "" ]]; then
+  case $(uname -m) in
+  i386) ARCH="386" ;;
+  i686) ARCH="386" ;;
+  x86_64) ARCH="amd64" ;;
+  arm) dpkg --print-architecture | grep -q "arm64" && ARCH="arm64" || ARCH="arm" ;;
+  esac
+fi
+
+export OS
+export ARCH
